6c0c5122ef46bf3252a04d0bd54c5e6cd39740bb
唐 学鋒 <gakuho_to@waku-2.com>
2018-02-01 13:28:25 +0900

    Add pod affinity.

--- hello-app/build.sh
+++ hello-app/build.sh
@@ -1,9 +1,22 @@
 #! /bin/sh
 
-SHA=`git describe --tags --always --dirty`
-DATE=`date -u +"%Y/%m/%d %H:%M:%S"`
-VERSION="Version: ${SHA}, build at: ${DATE}"
+GIT_SHA=`git describe --tags --always --dirty`
+BUILD_DATE=`date -u +"%Y/%m/%d %H:%M:%S"`
+VERSION="Version: ${GIT_SHA}, build at: ${BUILD_DATE}"
 
-docker build . --build-arg VERSION="${VERSION}"
+export GIT_SHA BUILD_DATE VERSION
 
-go build -ldflags "-X \"main.version=${VERSION}\"" main.go
+case $1 in
+    "build")
+        go build -ldflags "-X \"main.version=${VERSION}\"" main.go
+        ;;
+    "docker")
+        docker build . --build-arg VERSION="${VERSION}"
+        ;;
+    "deploy")
+        envsubst < manifests/helloweb-deployment.yaml | cat -
+        ;;
+    * )
+        echo "Unkown command: ${1}"
+        ;;
+esac
