cf4a2cb9de23b642430deb482b1079d56977db8c
tang <tang@artista.blue>
2018-02-02 18:09:18 +0900

    Use docker multi-stage to build binary.

--- hello-app/run.sh
+++ hello-app/run.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-GIT_SHA=`git describe --tags --always --dirty`
-BUILD_DATE=`date -u +"%Y/%m/%d %H:%M:%S"`
-VERSION="Version: ${GIT_SHA}, build at: ${BUILD_DATE}"
+if [ -z ${VERSION+x} ]; then
+    GIT_SHA=`git describe --tags --always --dirty`
+    BUILD_DATE=`date -u +"%Y/%m/%d %H:%M:%S"`
+    VERSION="Version: ${GIT_SHA}, build at: ${BUILD_DATE}"
 
-export GIT_SHA BUILD_DATE VERSION
+    export GIT_SHA BUILD_DATE VERSION
+fi
 
 COMMAND=$1
 shift 1
@@ -14,7 +16,7 @@ case ${COMMAND} in
         CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags "-X \"main.version=${VERSION}\"" $@
         ;;
     "docker")
-        docker build . $@
+        docker build . --build-arg VERSION="${VERSION}" $@
         ;;
     "deploy")
         envsubst < manifests/helloweb-deployment.yaml | cat - $@
