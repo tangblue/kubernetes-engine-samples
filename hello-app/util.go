97ae11f26c7768d1c2e4d8821698e78d9ee386eb
phoofeng <phoofeng@gmail.com>
2018-04-07 15:09:45 +0900

    Add SIGTERM handler.

--- hello-app/util.go
+++ hello-app/util.go
@@ -3,15 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-var version string
-var started time.Time
+var (
+	version string
+	started time.Time
+	sigs    chan os.Signal
+	termed  bool
+)
+
+func sigHandler() {
+	for sig := range sigs {
+		switch sig {
+		case syscall.SIGTERM:
+			fmt.Println("Received: SIGTERM")
+			termed = true
+			return
+		default:
+			fmt.Println("Received:", sig)
+		}
+	}
+}
 
 func init() {
 	fmt.Println("Version:", version)
 	started = time.Now()
+	sigs = make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+	go sigHandler()
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
@@ -21,12 +44,20 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 
 func readiness(w http.ResponseWriter, r *http.Request) {
 	errMsg := ""
+
+	if termed {
+		errMsg += "Terminated.\n"
+		http.Error(w, errMsg, http.StatusServiceUnavailable)
+		return
+	}
+
 	duration := time.Now().Sub(started)
 	if duration.Seconds() < 10 {
 		errMsg += "Database not ok.\n"
 		http.Error(w, errMsg, http.StatusServiceUnavailable)
-	} else {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
+		return
 	}
+
+	w.WriteHeader(200)
+	w.Write([]byte("ok"))
 }
